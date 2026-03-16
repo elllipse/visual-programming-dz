279c257be5ebc38af303196517aac487aeb40086
Mikhail <kostmi@bk.ru>
2026-03-16 16:43:31 +0700

    add coverage

--- lab4type/vitest.config.ts
+++ lab4type/vitest.config.ts
@@ -1,9 +1,20 @@
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    include: ['**/*.{test,spec}.ts']
+    include: ['**/*.{test,spec}.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',        
+      '**/*.js'           
+    ],
+    coverage: {
+      provider: 'v8',
+      include: ['**/*.ts'], 
+      exclude: ['**/*.test.ts', '**/*.spec.ts', '**/dist/**']
+    }
   }
 });
